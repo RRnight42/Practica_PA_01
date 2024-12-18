3dbb57e92bfef58bae7fe3ee007df01481ce0250
Daniel Mazo Sola <d.mazo.2023@alumnos.urjc.es>
2024-12-18 16:40:10 +0100

    Enum de levels y avances en la práctica.

--- Practica_01_PA_AlejandroMarin_RobertoRojas_DanielMazo_MiguelAmado/desarrollopa/GameScene.cpp
+++ Practica_01_PA_AlejandroMarin_RobertoRojas_DanielMazo_MiguelAmado/desarrollopa/GameScene.cpp
@@ -1,1 +1,38 @@
 #include "GameScene.h"
+
+
+void GameScene::Init() {
+
+	// configuraciones comunes de todos los niveles
+	// dibujar escena y demas tralla
+
+
+	switch (this->getLevel()) {
+	
+	case Level1:
+		break;
+	
+	case Level2:
+		break;
+
+	case Level3:
+		break;
+
+	case Level4:
+		break;
+
+	case Level5:
+		break;
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+}
--- Practica_01_PA_AlejandroMarin_RobertoRojas_DanielMazo_MiguelAmado/desarrollopa/GameScene.h
+++ Practica_01_PA_AlejandroMarin_RobertoRojas_DanielMazo_MiguelAmado/desarrollopa/GameScene.h
@@ -6,12 +6,18 @@ class GameScene : public Scene
 {
 private:
 
+	enum Level {Level1 , Level2 , Level3 , Level4, Level5};
 
-
-
+	Level level;
 
 public:
+	
+	inline Level getLevel() const { return this->level; }
 
+
+	GameScene(Level levelToSet) : level(levelToSet){}
+
+	void Init();
 
 
 };
